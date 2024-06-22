dd5afa03f8d401a2361e48f97a1c0b26c90781c9
unknown <kirkjholt@gmail.com>
2024-06-21 20:46:06 -0600

    started const and if's

---
+++ script.js
@@ -0,0 +1,14 @@
+const choices=('rock, paper, Scissors');
+const computeChoice =math.random(0,1)
+if (computerChoice <0.34) {
+    computerChoice =[0];
+}else if (computerChoice <=0.67) {
+    computerChoice = [1];
+}else {
+    computerChoice = [2];
+}
+
+window.alert("want to play Rock Paper Scissors?");
+prompt("R, P, S")
+if (window.confirm("rock beats scissors"))
+    alert("want to play again?")
